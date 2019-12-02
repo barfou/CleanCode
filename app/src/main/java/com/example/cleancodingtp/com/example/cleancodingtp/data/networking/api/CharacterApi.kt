d02acc501970978128f443f72ed8604fc147d88a
lpiem <lpiem@iutabgdin202-03.univ-lyon1.fr>
2019-12-02 15:22:10 +0100

    Add Test Api

--- app/src/main/java/com/example/cleancodingtp/com/example/cleancodingtp/data/networking/api/CharacterApi.kt
+++ app/src/main/java/com/example/cleancodingtp/com/example/cleancodingtp/data/networking/api/CharacterApi.kt
@@ -5,13 +5,15 @@ import com.example.cleancodingtp.com.example.cleancodingtp.data.Model.PaginatedR
 import retrofit2.http.GET
 import com.example.cleancodingtp.com.example.cleancodingtp.data.networking.api.CharacterApi.Companion
 import retrofit2.Response
+import retrofit2.http.Query
 
 interface CharacterApi {
 
     @GET(Companion.GET_ALL_CHARACTER_PATH)
         suspend fun getAllCharacter(
+        @Query("page") page :Int
 
-    ) : Response<List<Character>>
+    ) : Response<PaginatedResult<Character>>
 
     companion object {
         const val GET_ALL_CHARACTER_PATH = "character/"
